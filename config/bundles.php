500ff9b8e7970d36557c9c73bd031813d5deadab
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-06-17 16:43:01 +0200

    install OAuth2 Server Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    League\Bundle\OAuth2ServerBundle\LeagueOAuth2ServerBundle::class => ['all' => true],
 ];
--- docs/setup.md
+++ docs/setup.md
@@ -1,4 +1,10 @@
 # SETUP sso use symfony
 
+## install symfony
 - create composer.json
 - run ```composer install```
+
+## install OAuth2 Server Bundle
+```shell
+composer require league/oauth2-server-bundle
+``` 
